4955687590415ed94edc843922f1dfad738ee2fd
Metehan <mehmetmetehan.cavdar@agu.edu.tr>
2023-07-17 14:15:51 +0300

    history servis

--- historyApi/dbcon.js
+++ historyApi/dbcon.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://gliencherth:jbZpDteXdkXBhtVy@calcp.2xsojvd.mongodb.net/',{
+        await mongoose.connect('mongodb+srv://gliencherth:jbZpDteXdkXBhtVy@calcp.2xsojvd.mongodb.net/CalculatorIntern',{
             useUnifiedTopology:true,
             useNewUrlParser:true,
         });
--- historyApi/index.js
+++ historyApi/index.js
@@ -3,20 +3,27 @@ const connectDB=require('./dbcon');
 const mongoose= require('mongoose');
 const Input=require('./models/inputschema');
 const router = require('./router/dbrouter');
-
+const bodyParser = require('body-parser');
 connectDB();
 
+
+
 const server= express();
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: false }))
+
 
 server.get('/',(req,res)=> {
     res.send('Bu API Bimser Yaz Staji için geliştirilmiştir.');
 } );
 
-router()
+server.get('/history',router)
+
+server.post('/history',router);
 
 mongoose.connection.once('open', ()=>{
     console.log('Connected to MongoDB')
     server.listen(5000, () =>{
         console.log('Server running on: http://localhost:5000')
     });
-})
+});
