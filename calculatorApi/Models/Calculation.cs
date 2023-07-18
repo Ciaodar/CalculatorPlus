f13205c0a61e6c87d15c44e22d05c6cf3f105011
Ahmet Gürel <96393543+aegurel@users.noreply.github.com>
2023-07-19 00:12:48 +0300

    Update Calculation.cs

--- calculatorApi/Models/Calculation.cs
+++ calculatorApi/Models/Calculation.cs
@@ -7,8 +7,6 @@
         public int result { get; set; }
         public string signOperation { get; set; }
     }
-    var rabbitMQService = new RabbitMQService("amqps://ejejtxro:zt5zcNLY1EsDdMdKQ_KsIh9Q5AT3R7Fu@jackal.rmq.cloudamqp.com/ejejtxro");
-    var message = JsonConvert.SerializeObject(calculation);
-    rabbitMQService.SendMessage(message);
+
 }
 
