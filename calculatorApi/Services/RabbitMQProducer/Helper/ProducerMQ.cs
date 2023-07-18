bc6b05d42a2a6762f0d2169818077eb3f9f77e29
aegurel <ahmetensarg01@gmail.com>
2023-07-19 00:20:57 +0300

    rabbitmq producer v1

--- calculatorApi/Services/RabbitMQProducer/Helper/ProducerMQ.cs
+++ calculatorApi/Services/RabbitMQProducer/Helper/ProducerMQ.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using calculatorApi.Models;
+using RabbitMQ.Client;
 namespace calculatorApi.Services.RabbitMQProducer.Helper
 {
-    public class ProducerMQ
+    public class ProducerMQ : ISendMessage
     {
-        public ProducerMQ()
+        //var rabbitMQService = new RabbitMQService("amqps://ejejtxro:zt5zcNLY1EsDdMdKQ_KsIh9Q5AT3R7Fu@jackal.rmq.cloudamqp.com/ejejtxro");
+        //var message = JsonConvert.SerializeObject(calculation);
+        //rabbitMQService.SendMessage(message);
+
+        public void SendMessage(string message)
         {
+            var connection = RabbitHelper.GetConnection;
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("CalculationQueue", false, false, false, null);
+            var body = StringExtensions.GetBytes(message);
+            channel.BasicPublish("", "CalculationQueue", null, body);
         }
     }
 }
