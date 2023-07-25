79176096e274a8f9f685a5782f63f3e1dd903ecb
aegurel <ahmetensarg01@gmail.com>
2023-07-25 15:00:37 +0300

    socketfix

--- historyApi/services/Socket.js
+++ historyApi/services/Socket.js
@@ -3,7 +3,7 @@ let ALL_USERS = [];
 const SocketServer = (wss)=>{
     wss.on('connection',(ws)=>{
         ALL_USERS.push(ws);
-
+        console.log("yeni bağlantı");
         wss.on('close',(ws)=>{
             const id = ALL_USERS.indexOf(ws);
             ALL_USERS.slice(id,1);
@@ -12,6 +12,7 @@ const SocketServer = (wss)=>{
     })
 }
 function sendCalculationAll(message){
+    console.log(message);
     for(let i = 0;i<ALL_USERS.length;i++){
         ALL_USERS[i].ws.send(message);
     }
